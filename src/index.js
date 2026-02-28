2bf184f05df01b90d064b80bd60b6a2855fecabc
Khalil Aniba <khalil.aniba@issatm.ucar.tn>
2026-02-28 12:18:38 +0100

    seance1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavBar from './components/Navbar/NavBar';
+import Fotter from './components/Footer/Fotter';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    </NavBar />
     <App />
+    <Fotter />
   </React.StrictMode>
 );
 
